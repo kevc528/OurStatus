4807f14a998301a5a2ac7ed16eab56d5c1eb67dd
Kevin Chen <kevc528@seas.upenn.edu>
2020-07-04 15:13:13 -0500

    create account page

--- WebApp/src/app/users/create-account/create-account.component.ts
+++ WebApp/src/app/users/create-account/create-account.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Account } from '../../shared/model/account';
 import { AccountService } from '../account.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-create-account',
@@ -9,37 +10,59 @@ import { AccountService } from '../account.service';
 })
 export class CreateAccountComponent implements OnInit {
 
+  newAccount : Account = {
+    firstName: null,
+    lastName: null,
+    email: null,
+    username: null,
+    password: null,
+    friends: [],
+    groupIds: []
+  }
 
-  success: boolean = true;
+  confirmPassword: string = null;
+
+  error: boolean = false;
   errorMessage: string;
 
   constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
-    this.onCreateAccount();
+    
   }
 
-  onCreateAccount(): void {
-    // in the actual create account page, the account vars will be bound to angular form
-    let account = {
-      username: "test",
-      password: "Password1!",
-      firstName: "TestFirst",
-      lastName: "TestLast",
-      email: "test@gmail.com",
-      friends: [],
-      groupIds: []
-    } as Account;
-    // promise, then return to log in again
-    this.accountService.createAccount(account)
-      .then(function(ref) {
-        // this is the key for the document in the db
-        console.log(ref.id);
-        // window.location.href = 'http://www.youtube.com/watch?v=dQw4w9WgXcQ';
-      }, function() {
-        this.success = false;
-        this.errorMessage = "Account couldn't be created";
-      });
+  onSubmit(form: NgForm): void {
+    if (form.valid) {
+      if (this.newAccount.password == this.confirmPassword) {
+        let subscription = this.accountService.getAccount(this.newAccount.username).subscribe(
+          (res) => {
+            if (res.length == 0) {
+              subscription.unsubscribe();
+              this.accountService.createAccount(this.newAccount)
+                .then(function(ref) {
+                  window.location.href = 'login';
+                }, function() {
+                  this.error = true;
+                  this.errorMessage = "Account couldn't be created, try again later";
+                });
+            } else {
+              subscription.unsubscribe();
+              this.error = true;
+              this.newAccount.username = '';
+              this.errorMessage = 'Username already exists';
+            } 
+          }
+        )
+      } else {
+        this.error = true;
+        this.errorMessage = "Passwords don't match";
+        this.newAccount.password = '';
+        this.confirmPassword = '';
+      }
+    } else {
+      this.error = true;
+      this.errorMessage = "Please fix above errors";
+    }
   }
 
 }
