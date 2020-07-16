2eea6614b86371dc8623cca355dc5d726f2645e9
Kevin Chen <kevc528@seas.upenn.edu>
2020-07-15 22:55:24 -0500

    dist folder for aws deployement

--- WebApp/src/environments/environment.prod.ts
+++ WebApp/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB-CuxGBSwGbgsM88C97F4pU7jLSMBVavw",
+    authDomain: "ourstatus-71678.firebaseapp.com",
+    databaseURL: "https://ourstatus-71678.firebaseio.com",
+    projectId: "ourstatus-71678",
+    storageBucket: "ourstatus-71678.appspot.com",
+    messagingSenderId: "158499871442",
+    appId: "1:158499871442:web:9e3bdd6ade46d8fd792b0b",
+    measurementId: "G-RJ70VQGR3C"
+  }
 };
