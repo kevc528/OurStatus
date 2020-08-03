7af4c8d40049f4f3a8d7a506466fc14a415a249b
Jeffrey Deng <jeffdeng@berkeley.edu>
2020-08-02 21:28:44 -0500

    Started with pickers

--- OurStatusiOSApp/OurStatus/View Controller/HomeViewController.swift
+++ OurStatusiOSApp/OurStatus/View Controller/HomeViewController.swift
@@ -8,10 +8,40 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var groupTextField: UITextField!
+    @IBOutlet weak var calendarTextField: UITextField!
+    @IBOutlet weak var timeTextField: UITextField!
+    
+    let groups = ["Personal", "Development"]
+    
+    var pickerView = UIPickerView()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        
+        groupTextField.inputView = pickerView
+        groupTextField.textAlignment = .center
+        groupTextField.placeholder = groups[0]
+    }
+    
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return groups.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return groups[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        //Todo
     }
 }
